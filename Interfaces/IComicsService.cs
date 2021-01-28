614161f397a62419fe402ebc08ae3f1428c56ffd
Taiber009 <taiber009@mail.ru>
2021-01-28 20:21:56 +0300

    saved

---
+++ Interfaces/IComicsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ModelsLibrary;
+
+namespace Interfaces
+{
+
+    public interface IComicsService
+    {
+        PagedResult<ComicsModel> ComicsGetById(int id);
+
+        PagedResult<ComicsModel> ComicsGetAll();
+
+        PagedResult<ComicsModel> ComicsGetByPageAndPagecount(int page, int pagecount);
+    }
+}
